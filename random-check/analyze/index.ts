a3f9c8947f72f3b23ff9931cc264bbd780be74e9
Maxim Mazurok <maxim@mazurok.com>
2024-04-10 19:03:06 +1000

    sim returns

--- random-check/analyze/index.ts
+++ random-check/analyze/index.ts
@@ -1,4 +1,6 @@
 import Chart from "chart.js/auto";
+import { differenceInSeconds } from "date-fns";
+import { ma } from "moving-averages";
 import {
   getHistoricalBalanceData,
   getHistoricalData,
@@ -8,11 +10,11 @@ import {
   calculateChances,
   convertFractionOddsToChance,
   convertFractionOddsToDecimal,
+  determineWinner,
+  generateLineUp,
   isValidDataSource,
   sortByOdds,
 } from "../../utils";
-import { ma } from "moving-averages";
-import { differenceInSeconds } from "date-fns";
 
 const updateCharts = async (dataSource: unknown) => {
   if (!isValidDataSource(dataSource)) throw new Error("Invalid data source");
@@ -26,11 +28,12 @@ const updateCharts = async (dataSource: unknown) => {
     historicalBalanceDataRes.json(),
   ]);
 
+  movingAverageChart(chart0, historicalBalanceData);
   doIt(chart1, "horse", historicalData);
   doIt(chart2, "group", historicalData);
   doReturns(chart3, historicalData);
   doReturnsLine(chart4, historicalData);
-  movingAverageChart(chart0, historicalBalanceData);
+  doReturnsLineSim(chart5);
 };
 
 const movingAverageChart = async (
@@ -85,6 +88,7 @@ const chart1 = initChart(1);
 const chart2 = initChart(2);
 const chart3 = initChart(3);
 const chart4 = initChart(4, "line");
+const chart5 = initChart(5, "line");
 
 const doIt = async (
   chart: Chart,
@@ -392,6 +396,127 @@ const doReturnsLine = async (
       requestAnimationFrame(() => next(index + 1));
       // next(index + 1);
     }
+  };
+
+  next();
+};
+
+const doReturnsLineSim = async (chart: Chart) => {
+  if (!horses) {
+    horses = (await fetch("/api/horses").then((res) => res.json())) as Awaited<
+      ReturnType<typeof getHorses>
+    >;
+  }
+
+  setInterval(() => {
+    chart.update("none");
+  }, 1000);
+
+  chart.options.scales = {
+    y: {
+      min: 0,
+      max: 3,
+    },
+  };
+  chart.options.elements = {
+    point: {
+      radius: 0,
+    },
+  };
+  // @ts-ignore
+  chart.options.spanGaps = true;
+
+  chart.data = {
+    labels: [],
+    datasets: [],
+  };
+
+  const localData = {
+    returns: [] as number[][],
+    playedGames: {},
+    wonGames: {},
+  };
+
+  function getColor(value: number) {
+    const hue = ((1 - value) * 120).toString(10);
+    const transparency = value.toString(10);
+
+    return `hsla(${hue},100%,50%,${transparency})`;
+  }
+
+  for (let i = 0; i < horses.length; i++) {
+    localData.playedGames[i] = 0;
+    localData.wonGames[i] = 0;
+
+    const color = getColor(
+      convertFractionOddsToChance(
+        horses[i].oddsNumerator,
+        horses[i].oddsDenominator
+      ) * 2
+    );
+
+    chart.data.datasets.push({
+      label: `#${i}`,
+      data: [] as number[],
+      borderColor: color,
+      backgroundColor: color,
+    });
+  }
+
+  const next = () => {
+    const iterate = () => {
+      const index = chart.data.labels.length;
+
+      chart.data.labels!.push(index);
+
+      // const { lineUp, winner } = historicalData[index];
+      const lineUp = generateLineUp(horses);
+      const winner = determineWinner(lineUp);
+
+      for (let i = 0; i < horses.length; i++) {
+        localData.returns[index] = [];
+        localData.returns[index][i] = 0;
+      }
+
+      lineUp.forEach((horse, i) => {
+        const id = horses.findIndex((x) => x.name === horse.name);
+
+        if (!localData.returns[id]) localData.returns[index][id] = 0;
+        if (!localData.playedGames[id]) localData.playedGames[id] = 0;
+        if (!localData.wonGames[id]) localData.wonGames[id] = 0;
+
+        localData.playedGames[id] += 1;
+
+        if (JSON.stringify(horse) === JSON.stringify(winner)) {
+          localData.wonGames[id] += 1;
+        }
+
+        localData.returns[index][id] =
+          (convertFractionOddsToDecimal(
+            horse.oddsNumerator,
+            horse.oddsDenominator
+          ) *
+            localData.wonGames[id]) /
+          localData.playedGames[id];
+      });
+
+      for (let i = 0; i < horses.length; i++) {
+        // @ts-ignore
+        chart.data.datasets[i].data[index] =
+          // localData.returns[index][i] || chart.data.datasets[i].data[index - 1];
+          localData.returns[index][i] || undefined;
+      }
+    };
+
+    for (let i = 0; i < 50; i++) {
+      iterate();
+    }
+
+    // if (index < historicalData.length - 1) {
+    setTimeout(next, 1000);
+    // requestAnimationFrame(() => next());
+    // next(index + 1);
+    // }
   };
 
   next();
