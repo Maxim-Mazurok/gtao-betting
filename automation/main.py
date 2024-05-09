8bffaa67a84b43021b18a599a4cad6b6e13e24f0
Maxim Mazurok <maxim@mazurok.com>
2024-05-09 17:35:46 +1000

    fix?

--- automation/main.py
+++ automation/main.py
@@ -255,7 +255,9 @@ def click_in_the_middle_of_crop(crop_coords, click_type='down-and-up'):
 
         # Move the mouse to the center point and click
         pydirectinput.moveTo(screen_center_x, screen_center_y)
-        time.sleep(0.1)  # Small delay
+
+        # increase delay, looks like sometimes the cursor is still on the "Bet Again" button which is actually "Rules" button by that time, not sure tho
+        time.sleep(0.5)
 
         if click_type == 'down-and-up':
             pydirectinput.press('enter')
